be40d768f9e0802faa32dcc97ef4949bdf48b04f
jaime <jamesowiti24@gmail.com>
2024-09-05 10:14:50 +0300

    Added TOP fibonnaci

---
+++ fibonnacci.js
@@ -0,0 +1,13 @@
+function fibonacci(n) {
+  let fibsArray = [0, 1];
+  for (let i = 0; i < n; i++) {
+    console.log(
+      fibsArray[fibsArray.length - 2] + fibsArray[fibsArray.length - 1]
+    );
+    fibsArray.push(
+      fibsArray[fibsArray.length - 2] + fibsArray[fibsArray.length - 1]
+    );
+  }
+}
+
+fibonacci(6);
