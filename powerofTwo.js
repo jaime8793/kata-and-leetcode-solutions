01f54381ecb962322312c18abd3394fbb4967601
jaime <jamesowiti24@gmail.com>
2024-11-24 21:36:19 +0300

    You will make it

---
+++ powerofTwo.js
@@ -0,0 +1,12 @@
+Math.log(2);
+function isPowerOfTwo(n) {
+  //.. should return true or false ..
+  console.log(Math.log2());
+  if (Math.log2(n) % 1 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+isPowerOfTwo();
