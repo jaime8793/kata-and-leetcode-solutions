f84459c93ecc80aacc6011ab157986eab69f8411
jaime <jamesowiti24@gmail.com>
2024-12-11 23:42:22 +0300

    Appearances

---
+++ wearCroptop.js
@@ -0,0 +1,9 @@
+function shouldIWearCropTop(){
+    if (flatTummy === true) {
+        return "You can wear a cropTop"
+    } else {
+        return "Tuheshimiane please😂"
+    }
+}
+
+shouldIWearCropTop();
