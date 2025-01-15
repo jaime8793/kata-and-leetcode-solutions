faaea53fdac51e8c666e0e622f076b93eb50118d
jaime <jamesowiti24@gmail.com>
2025-01-15 21:50:18 +0300

    Added splited string

---
+++ breakCamelCasing.js
@@ -0,0 +1,15 @@
+function solution(string) {
+    let splitedString = string.split("")
+    let newString = []
+    console.log(splitedString)
+    for (i = 0; i < splitedString.length; i++){
+        if (/[A-Z]/.test(splitedString[i])) {
+            newString.push(' ')
+            newString.push(splitedString[i])
+        } else {
+            newString.push(splitedString[i])
+        }
+    }
+  return newString.join("");
+}
+console.log(solution("camelCasingTest"));
