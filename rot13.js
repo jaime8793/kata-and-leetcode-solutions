9d7da42f853fb301d2ba399e6133892485be35ed
jaime <jamesowiti24@gmail.com>
2025-06-27 15:12:02 +0300

    Added rot13 cipher

---
+++ rot13.js
@@ -0,0 +1,17 @@
+function rot13(message) {
+  //your code here
+  const splitedMessage = message.split("");
+  const rot13Message = [];
+    splitedMessage.map((letter) => {
+        if (letter.charCodeAt(0) >= 65 && letter.charCodeAt(0) <= 90) {
+            rot13Message.push((letter.charCodeAt(0) + 13) % 65);
+        } else if (letter.charCodeAt(0) >= 97 && letter.charCodeAt(0) >= 122) {
+            rot13Message.push((letter.charCodeAt(0) + 13) % 97);
+        } else {
+            rot13Message.push(letter.charCodeAt(0));
+        }
+    })
+  console.log(rot13Message);
+}
+
+rot13("Cream")
